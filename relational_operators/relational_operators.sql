0313a59af4c0c274718be764af6524fb35bfb5da
sriganth12 <sriganth882@gmail.com>
2025-08-13 15:30:09 +0530

    corrected code

--- relational_operators/relational_operators.sql
+++ relational_operators/relational_operators.sql
@@ -1,47 +1,55 @@
-set SERVEROUTPUT on;
+SET SERVEROUTPUT ON;
 DECLARE
-  a number(2):=20;
-  b number(2):=20;
+  a NUMBER(2) := 20;
+  b NUMBER(2) := 20;
 BEGIN
-  if (a<b) THEN
+  IF (a < b) THEN
     DBMS_OUTPUT.PUT_LINE('a is less than b');
   ELSE
     DBMS_OUTPUT.PUT_LINE('a is not less than b');
   END IF;
-  if (a>b) THEN
+  
+  IF (a > b) THEN
     DBMS_OUTPUT.PUT_LINE('a is greater than b');
   ELSE
-    dbms_output.put_line('a is not greater than b');
-  end if;
-  if (a=b) THEN
-   DBMS_OUTPUT.PUT_LINE('a is equal to b');
-  ELSE
-   DBMS_OUTPUT.put_line('a is not equal to b');
-  end if;
-  if (a<=b) THEN
-   DBMS_OUTPUT.PUT_LINE('a is less than b or equal to b');
-  else 
-   DBMS_OUTPUT.PUT_LINE('a is not less than b or equal to b');
-  end if;
-  if (a>=b) THEN
-   DBMS_OUTPUT.PUT_LINE('a is greater than b or equal to b');
-  ELSE 
-   DBMS_OUTPUT.PUT_LINE('a is not greater thab b or equal to b');
-  end if;
-  if (a=b) THEN
-   DBMS_OUTPUT.PUT_LINE('a is equal to b');
-  else 
-   DBMS_OUTPUT.PUT_LINE('a is not equal to b');
-  end if;
-  if (a!=b) THEN
-   dbms_output.put_line("a is not equal to b");
-  end if;
-  if (a <> b) THEN
-   dbms_output.put_line("a is not equal to b");
-  end if;
-  if (a ~= b) THEN
-   dbms_output.put_line("a is not equal to b");
-  end if;
-end;
-/
+    DBMS_OUTPUT.PUT_LINE('a is not greater than b');
+  END IF;
   
+  IF (a = b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is equal to b');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('a is not equal to b');
+  END IF;
+  
+  IF (a <= b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is less than b or equal to b');
+  ELSE 
+    DBMS_OUTPUT.PUT_LINE('a is not less than b or equal to b');
+  END IF;
+  
+  IF (a >= b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is greater than b or equal to b');
+  ELSE 
+    DBMS_OUTPUT.PUT_LINE('a is not greater than b or equal to b');
+  END IF;
+  
+  IF (a = b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is equal to b');
+  ELSE 
+    DBMS_OUTPUT.PUT_LINE('a is not equal to b');
+  END IF;
+  
+  IF (a != b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is not equal to b');
+  END IF;
+  
+  IF (a <> b) THEN
+    DBMS_OUTPUT.PUT_LINE('a is not equal to b');
+  END IF;
+  
+  -- Note: ~= is not a valid comparison operator in PL/SQL
+  -- IF (a ~= b) THEN
+  --   DBMS_OUTPUT.PUT_LINE('a is not equal to b');
+  -- END IF;
+END;
+/
